2e8c4d170c0913bfc985c44b217e6706546c9805
danutapfeiffer <danutapfeiffer@hotmail.com>
2025-12-03 12:34:51 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,9 +4,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-project = 'Look Webroot'
+project = 'System Mechanic IOLO Live'
 copyright = '2025'
-author = 'Leigh Behnke'
+author = 'Ginny Rorby'
 
 release = '1.0'
 
@@ -21,4 +21,3 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
-
